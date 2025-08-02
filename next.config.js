52f343caf66a567ed0252e5b09e4cfbb965e194d
Luke Auderer <auderer@iastate.edu>
2025-08-02 10:50:31 -0500

    editing next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // <-- required for next export
-  images: {
-    unoptimized: true, // <-- avoids error with next/image
+  eslint: {
+    ignoreDuringBuilds: true, // disable blocking on eslint errors
   },
-  basePath: '/dev-portfolio', // required if hosted at github.com/username/YOUR-REPO-NAME
-  trailingSlash: true,         // improves GitHub Pages compatibility
 };
 
 module.exports = nextConfig;
