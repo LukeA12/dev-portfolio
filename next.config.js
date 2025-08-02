194f896b055a44aafe35c403dd0fe068b024a59c
Luke Auderer <auderer@iastate.edu>
2025-08-02 10:27:32 -0500

    deploying to github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export', // <-- required for next export
+  images: {
+    unoptimized: true, // <-- avoids error with next/image
+  },
+  basePath: '/dev-portfolio', // required if hosted at github.com/username/YOUR-REPO-NAME
+  trailingSlash: true,         // improves GitHub Pages compatibility
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
