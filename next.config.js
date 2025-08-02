b180a7c79ef78e5ac6f9fe348f9cefbb6d4aab0f
Luke Auderer <auderer@iastate.edu>
2025-08-02 12:40:44 -0500

    testing if adding export works

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // <-- required for next export
+  images: {
+    unoptimized: true, // <-- avoids error with next/image
+  },
+  basePath: '/dev-portfolio', // required if hosted at github.com/username/YOUR-REPO-NAME
+  trailingSlash: true,         // improves GitHub Pages compatibility
   eslint: {
     ignoreDuringBuilds: true, // disable blocking on eslint errors
   },
