88923ac63a71b8a0ca5adb6f9df2de1e72b12fca
LukeA12 <108907115+LukeA12@users.noreply.github.com>
2026-06-03 17:17:28 -0700

    June 2026 Updates (#8)

--- components/about.jsx
+++ components/about.jsx
@@ -10,31 +10,32 @@ export default function About (props) {
                 <h1 className="mb-5 text-4xl">
                 About <span className="font-bold">Me</span>
                 </h1> 
-                <p className="my-5 text-lg lg:pr-24">I&apos;m a Senior majoring in Computer Engineering at Iowa State University, 
-                  and I grew up in Ankeny, Iowa. In my free time, I enjoy fishing, working out, and spending time with friends. 
-                  I have interned at both John Deere (part-time during the school year and full-time during the summer) and SpaceX, 
+                <p className="my-5 text-lg lg:pr-24">I recently graduated from Iowa State University with a Bachelor's in Computer 
+                  Engineering and will be returning this fall to pursue a Master's in Artificial Intelligence and Machine Learning.
+                  I'm originally from Ankeny, Iowa, and outside of engineering I enjoy fishing, running, working out, and spending time with friends. 
+                  I have interned at Google, SpaceX, and John Deere (part-time during the school year and full-time during the summer), 
                   gaining hands-on experience in embedded systems, software engineering, and machine learning within large-scale 
                   engineering projects. Throughout college, I’ve worked part-time as a year-round intern at John Deere, while also 
                   working as an undergraduate researcher in Iowa State’s Advanced Manufacturing Lab. There, I’m leading a project to 
                   develop a low-cost structured light 3D scanner. You can find more details about my projects and experience below.</p>
                 <h3 className="mb-2 text-l">
-                <span className="font-bold">Languages</span>: Python, Java, C++, C, Swift, JavaScript (React.js, Node.js), HTML/CSS, MATLAB
+                <span className="font-bold">Languages</span>: Python, Java, C++, C, JavaScript (React.js, Node.js), HTML/CSS, MATLAB, SQL
                 </h3>
                 <h3 className="mb-2 text-l">
-                <span className="font-bold">Tools</span>: Git, Linux, OpenCV, CUDA, PyTorch, TensorFlow, AWS, SQL, Qt, CMake, Gradle, Bazel, Android SDK, Ansible, Docker, Grafana
+                <span className="font-bold">Tools</span>: Git, Linux, Docker, OpenCV, PyTorch, CUDA, JAX/Pallas, vLLM, MLflow, Databricks, AWS, Voxel51, Ansible, Grafana, Qt, CMake, ROS
                 </h3>
                 <h3 className="mb-2 text-l">
-                <span className="font-bold">Competencies</span>: Data Structures, Algorithms, OOP, Embedded Systems(UART, ADC, PWM, CAN), REST APIs, Multithreading, Networking (TCP/IP)
+                <span className="font-bold">Competencies</span>: Data Structures, Algorithms, OOP, Embedded Systems (UART, ADC, PWM, CAN), REST APIs, Multithreading, Networking (TCP/IP), 
+                Computer Vision, Machine Learning
                 </h3>
                 <h3 className="mb-2 text-l">
                 <div>
                 <span className="font-bold">Engineering Experience</span>:
                 <ul className="list-disc list-inside mt-1">
-                  <li>Software Engineering Intern @ SpaceX</li>
-                  <li>Machine Learning and Robotics Engineering Intern @ John Deere</li>
-                  <li>Design Engineering Intern @ John Deere</li>
-                  <li>Embedded Software Engineering Intern @ John Deere</li>
+                  <li>Machine Learning Intern @ Google</li>
+                  <li>Former Intern @ SpaceX and John Deere</li>
                   <li>Research Assistant @ Iowa State&apos;s Advanced Manufacturing Lab</li>
+                  <li>Computer Engineering & AI @ Iowa State University</li>
                 </ul>
               </div>
                 </h3> 
